dc4a5b215a968db08dff02a89a8ee2432cc24cf5
Eric Wang <twang173@ucsc.edu>
2024-10-23 13:45:38 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,18 @@ import "./style.css";
 const APP_NAME = "Hello";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
+const Canvas = document.getElementById ("myCanvas") as HTMLCanvasElement;
+const ctx = Canvas.getContext("2d");
+
+if(ctx){
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 256, 256);
+};
+
+const Header = document.createElement("h1");
+Header.innerHTML = APP_NAME;
+app.append(Header);
+
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
